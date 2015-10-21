b8db881abf5abe5ea7fe47273e0a5589592b3225
Constantin Clauzel <stantin04@gmail.com>
2015-10-21 18:15:11 +0200

    moved attachEvent outside onReady, cleaner

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
 'use strict';
 
-'use strict';
-
 let newClient = require('rotonde-client/src/Client');
 
-let client = newClient('ws://localhost:4224/');
+let client = newClient('ws://192.168.2.9:4224/');
 
 let openaction = {
   port: '/dev/ttyAMA0',
   baudrate: 9600,
 };
 
+client.eventHandlers.attach('SERIAL_PORTMESSAGE', (event) => {
+  console.log(event);
+});
+
 client.onReady(() => {
-  client.bootstrap({'SERIAL_OPEN': openaction}, ['SERIAL_OUTPUT']).then((events) => {
-    console.log('onready', events);
-    client.eventHandlers.attach('SERIAL_PORTMESSAGE', (event) => {
-      console.log(event);
-    });
+  client.bootstrap({'SERIAL_OPEN': openaction}, ['SERIAL_OUTPUT'], ['SERIAL_PORTMESSAGE']).then((events) => {
+    let serialOutputEvent = events[0].data;
+    if (serialOutputEvent.Cmd == 'Open') {
+      console.log('port open, start listening for messages');
+    }
   }, (error) => {
     console.log('error', error);
   });
