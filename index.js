7e99d72806f7c6bf037edaa5fe7faafee64119aa
Charles <charles@Georgess-MacBook-Pro.local>
2015-11-13 12:55:08 +0100

    Adapted module for rotonde-serial

--- index.js
+++ index.js
@@ -2,75 +2,76 @@
 
 let newClient = require('rotonde-client/src/Client');
 
-let client = newClient('ws://192.168.1.50:4224/');
+let client = newClient('ws://rotonde:4224/');
+
+const tty = '/dev/tty.usbserial-AH02LTLV';
 
 let openaction = {
-  port: '/dev/ttyAMA0',
-  baudrate: 9600,
+  port: tty,
+  baud: 115200,
 };
 
 let scanned = false;
 let RFID_tag = '';
 
-client.eventHandlers.attach('SERIAL_PORTMESSAGE', (event) => {
+client.eventHandlers.attach('SERIAL_READ', (event) => {
   
-  console.log(event);
-  let packet_data = event.data.D;
+  
+  if(event.data.port !== tty){
+    //console.log(event);
+    return;
+  }
 
-  if(event.data.D.startsWith( '\u0002')) {
-    console.log("startstring");
-    packet_data = packet_data.slice(1, packet_data.length);
+  let packet_data = new Buffer(event.data.data , 'base64');
+  let ascii_data = packet_data.toString();
+
+  if(ascii_data.startsWith( '\u0002')) {
+    ascii_data = ascii_data.slice(1, ascii_data.length);
     RFID_tag = '';
     scanned = false;
   }
-  if(event.data.D.endsWith( '\u0003' )) {
+  if(ascii_data.endsWith( '\u0003' )) {
     console.log("stopstring");
-    packet_data = packet_data.slice(0, packet_data.length-1);
+    ascii_data = ascii_data.slice(0, ascii_data.length-1);
     scanned = true;
   }
 
-  RFID_tag += packet_data;
+  RFID_tag += ascii_data;
 
   if(scanned) {
     scanned = false;
     check_rfid_data(RFID_tag);
-    //send_rfid_event();
     RFID_tag = '';
   }
 });
 
-function check_data(data) {
-  for (let i = 0 ; i < data.length ; i++) {
-    console.log("data " + i + " : " , data.charAt(i));
-  }
+//send RFID event to rotonde
+function send_rfid_event(tag) {
+
+  client.sendEvent('RFID_RECEIVED' , {tag: tag});
+  console.log("RFID RECEIVED : " + tag);
 }
 
-//send data to rotonde
-function send_rfid_event() {
-
-  client.sendEvent('RFID_RECEIVED' , {tag: RFID_tag});
-  console.log("RFID RECEIVED : " + RFID_tag);
-}
-
-//send data to rotonde
+//Check data integrity
 function check_rfid_data(tag) {
 
-  console.log(tag.length);
   //return error if length of data is not even
   if(tag.length %2 === 1){
     return console.log("Error in card number reception");
   }
 
-  let checksum = 0; //= tag.substr(tag.length() -1);
+  //Calculate checksum
+  let checksum = 0;
   for (let i = 0; i < tag.length-2; i+=2) { 
-    let temp = '0x' + tag.substr(i , 2);
-    checksum = checksum ^ parseInt(temp);
+    let temp = parseInt(tag.substr(i , 2) , 16);
+    checksum = checksum ^ temp;
   }
 
-  console.log(checksum , tag.substr(tag.length -2 , 2));
   if(checksum === parseInt(tag.substr(tag.length -2 , 2) , 16)){
     console.log("checksum ok");
-    send_rfid_event();
+    //Remove the checksum from the car number before sending it
+    tag = tag.slice(0, tag.length -2);
+    send_rfid_event(tag);
   } else {
     console.log("checksum not ok");
   }
@@ -84,14 +85,24 @@ client.addLocalDefinition('event' , 'RFID_RECEIVED' , [
     },
   ]);
 
-client.onReady(() => {
-  client.bootstrap({'SERIAL_OPEN': openaction}, ['SERIAL_OUTPUT'], ['SERIAL_PORTMESSAGE']).then((events) => {
+function openport() {
+  client.bootstrap({'SERIAL_OPEN': openaction}, ['SERIAL_STATUS'] , ['SERIAL_READ']).then((events) => {
     let serialOutputEvent = events[0].data;
-    if (serialOutputEvent.Cmd == 'Open') {
+    if (serialOutputEvent.status == 'SUCCESS') {
       console.log('port open, start listening for messages');
-      
-      console.log('port open, sent module definitions');
     }
+  }, (error) => {
+    console.log('error', error);
+  });
+}
+
+
+
+client.onReady(() => {
+  client.bootstrap({'SERIAL_CLOSE': {port:tty}}, [] , ['SERIAL_STATUS']).then((events) => {
+    console.log('sent close');
+    //Wait for the serial module to close the connection if already open (1s delay)
+    client.eventHandlers.makePromise('SERIAL_STATUS' , 1500).then( openport , openport);
   }, (error) => {
     console.log('error', error);
   });
